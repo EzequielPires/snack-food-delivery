39d8cb2c90fdaf2f75dbd0ff7780d6bc1dc31dfd
EzequielPires <ezequiel.pires082000@gmail.com>
2022-05-03 12:46:30 -0300

    Commit

--- src/pages/restaurantes/index.tsx
+++ src/pages/restaurantes/index.tsx
@@ -1,16 +1,20 @@
-import { FaStar } from 'react-icons/fa';
+import { FaChevronDown, FaFilter, FaRunning } from 'react-icons/fa';
 import { CardRestaurant } from '../../components/CardRestaurant';
 import { Footer } from '../../components/Footer';
 import { HeaderFluid } from '../../components/HeaderFluid';
 import { SwiperBestRestaurants } from '../../components/SwiperBestRestaurants';
 import { SwiperCategories } from '../../components/SwiperCategories';
+import { SwiperFilter } from '../../components/SwiperFilter';
 import styles from './styles.module.scss';
 
 export default function Restaurantes() {
     return (
         <div className={styles.container}>
             <HeaderFluid />
-            <div className="container pt-5 mb-5">
+            <div className="container pt-3 mb-5">
+                <div className={styles.filter + " d-flex gap-4"}>
+                    <SwiperFilter />
+                </div>
                 <SwiperCategories />
             </div>
             <SwiperBestRestaurants title={'Famosos no SnackDelivery  '} />
